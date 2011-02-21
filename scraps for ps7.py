08c41d1c8a6c22535322c678048f56ed065c1a2c
Scott Brenner <scott@scottbrenner.com>
2011-02-20 19:24:12 -0500

    ps7: Completed a first draft of answers.  I need to review them again.
    Also included a scrap file of code I ran while answering ps7.

--- scraps for ps7.py
+++ scraps for ps7.py
@@ -88,17 +88,31 @@ print "Total time to complete intersect(s1, s2):", end_time - start_time
 
 
 def swap0(s1, s2):
-	assert type(s1) == list and type(s2) == list
-	tmp = s1[:]
-	s1 = s2[:]
-	s2 = tmp
-	return
+    assert type(s1) == list and type(s2) == list
+    print 'In swap0. s1, s2:', s1, s2
+    tmp = s1[:]
+    print 'In swap0. s1, s2, tmp:', s1, s2, tmp
+    s1 = s2[:]
+    print 'In swap0. s1, s2, tmp:', s1, s2, tmp
+    s2 = tmp
+    print 'In swap0. s1, s2, tmp:', s1, s2, tmp
+    return
 
 s1 = [1]
 s2 = [2]
+print 's1, s2:', s1, s2
 swap0(s1, s2)
 print s1, s2
 
+def rev(s):
+	assert type(s) == list
+	for i in range(len(s)/2):
+		tmp = s[i]
+		s[i] = s[-(i+1)]
+		s[-(i+1)] = tmp
+s =	[1,2,3]
+rev(s)
+print s
 
 
 
