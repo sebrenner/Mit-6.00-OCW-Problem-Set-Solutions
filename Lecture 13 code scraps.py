0657a21e024bb0bbf465dcd75130fcdc8e646815
Scott Brenner <scott@scottbrenner.com>
2011-02-27 13:24:58 -0500

    PS8: lecture code scraps is now building a memo, with the existing path.  it is not working correctly, but it isnt' throwing any errors, either.  yea.

--- Lecture 13 code scraps.py
+++ Lecture 13 code scraps.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-untitled.py
+lecture code scraps.py
 
 Created by Scott Brenner on 2011-02-25.
 Copyright (c) 2011 Scott Brenner. All rights reserved.
@@ -12,75 +12,8 @@ import os
 numCalls = 0
 num2Calls = 0
 
-## This code does not produce the correct answer.
-def fib(n):
-    global numCalls
-    numCalls += 1
-    print 'fib called with', n
-    if n <= 1:
-        return 1
-    else:
-        return fib(n-1) + fib(n-2)
-
-
-def fib1(n):
-    memo = {0:1,1:1}
-    return fastFib(n,memo)
-
-def fastFib(n, memo):
-    global numCalls
-    numCalls += 1
-    print 'fib1 called with', n
-    if not n in memo:
-        memo[n] = fastFib(n-1, memo) + fastFib(n-2, memo)
-    return memo[n]
-
-# numCalls = 0
-# n = 16
-# res = fib(n)
-# print 'fib of %i = %i. \tnumbCalls = %i.' %(n, res, numCalls)
-# numCalls = 0
-# res = fib1(n)
-# print 'fib of %i = %i. \tnumbCalls = %i.' %(n, res, numCalls)
-
-
-
-# dynamic programming functions from lecture handout
-def maxVal(w, v, i, aW):
-    """
-    Returns the value of the optimal combination of items that doesn't exceed aW
-    But what about the combination itself?  How do we get that?
-    
-    With this implementation this function is doing the same work over and over again.  For example it call maxVal with the same i, aW value many times.  Note that w and v are constant.
-    
-     """
-    print 'maxVal called with:', i, aW
-    global numCalls
-    numCalls += 1
-    
-    # base case: check to see if we are at the end (starting at tail)
-    if i == 0:                          # at end (starting at tail) of the vector
-        if w[i] <= aW:                  # if there is room for it add it 
-            return v[i]                 # return its value
-        else: return 0                  # otherwise return 0, i.e., too big to take
-    
-    """
-    do this if we are not at the end, starting at tail.
-    don't select the current item move down the vector and 
-    set without_i to the result of maxval of this next item
-    this is the statement that recurses the function until it reaches the vase case case and begins returning back up the recursed functions.
-    """
-    without_i = maxVal(w, v, i-1, aW)
-    
-    
-    """
-    If the weight of i exceeds the availble work, then return the value of the previously evaluated item (the item closer to the 0th item).  Basically, if there isn't room for this item then the
-    """
-    if w[i] > aW:
-        return without_i
-    else:
-        with_i = v[i] + maxVal(w, v, i-1, aW - w[i])
-    return max(with_i, without_i)
+current_combo = []
+best_combo = []
 
 def tryMaxVal():
     w = [1,5,3,2,4]
@@ -101,7 +34,7 @@ def maxVal0(w, v, i, aW):
     
     SCOTT: CONSIDER ADDING a procedure that reduces the length catalog by elmiinating class with workloads that exceed max work.  Also consider simplifying this function to just take the catalog and aW. That way the prep work can be 'hidden' from the user.
     """
-    print "in maxVal0"
+    
     m = {}
     return fastMaxVal(w, v, i, aW, m)
 
@@ -117,38 +50,50 @@ def fastMaxVal(w, v, i, aW, m):
     *** This function returns the highes value but how do you find the i's that were choosen.  SUGGESTION:  as each itme is taken along the take branch add the i (index) to a dictionary called best choice. 
     
     """
+    #   These are just here for counting the number of recursions.
     global num2Calls
     num2Calls += 1
-    
+    print
+    print "i,aW", i,aW    
     #   Check if this index, remaining workload pair has been evaluated.
     #   If it has, return the previously calculated result(~vi).
-    try: return m[(i, aW)]      #   use the try: except scheme because attempting to access a key that doesn't exist returns an error
+    try: return m[(i, aW)],['room']    #   use the try: except scheme because attempting to access a key that doesn't exist returns an error
     except KeyError:
         
         ################ Base case.  
         if i == 0:
+            print "in base case"
             if w[i] <= aW:          # If there is a enough aW for the w[0] then
+                print "take base" 
                 m[(i, aW)] = v[i]   # create dictionary entry for 0th, aw with value of 0th itme
-                return v[i]         # return value of 0th itme
+                return v[i], [0]         # return value of 0th item and i itself.
             else:
+                print "don't take base"
                 m[(i, aW)] = 0      # not enough room in aW for 0th item, return 0
-                return 0
+                return 0, []        # return 0 and an empty string.
                 
         ################ Recurse with the next item closer to 0th.
         ################ This is basically builing the results for all the don't-take branches.
-        without_i = fastMaxVal(w, v, i-1, aW, m)    #   Set without_i to the value returned by fastMaxVal for the next i.
-        
+        print "not in base.  Now at i =", i
+        without_i, i_list = fastMaxVal(w, v, i-1, aW, m)   #   Set without_i to the value returned by fastMaxVal for the next i.
+        print "back up from base."
+        print "without_i, i_list:\t", without_i, i_list
         if w[i] > aW:               # If there isnt a enough w[i] then
-            m[(i, aW)] = without_i  # This is a dead end with this aW.  Make a dictionary entry.
-            return without_i        # Return value of of path to this point.
+            print "not enough room aW for w[i]", w[i] > aW
+            m[(i, aW)] = without_i      # This is a dead end with this aW.  Make a dictionary entry.
+            return without_i, i_list  # Return value of of path to this point and list of i's to this point.
             
-        else:               # There is room in aW for w[i].
+        else:       # There is room in aW for w[i].
             #   Recursibely build the do-take branches
             #   Set with_i value to value of just selected item and add the value of the downstream branch/decisions.
-            with_i = v[i] + fastMaxVal(w, v, i-1, aW - w[i], m)
+            print "enough room aW for w[i].  w[i], aW, i:\t", w[i], aW, i
+            with_i, gant = fastMaxVal(w, v, i-1, aW - w[i], m)
+            with_i += v[i]
+            gant += [i]
+            i_list = gant
         res = max(with_i, without_i)    # Decide whether choosing the item produces a higher value than not choosing the item.
         m[(i, aW)] = res                # Add a dictionary key for the better decision.
-        return res                      # Return the higher value.
+        return res, gant            # Return the higher value, and the selected i's.
 
 
 
@@ -157,8 +102,8 @@ def tryMaxVals(n):
     v = [15,10,9,5,5]
     w = [2,1,2,3,4,1,2,3,4]
     v = [8,15,10,9,5,15,10,9,5]
-    # w = [7,4,3,4,5,6,7,8,8,9,9,5,1,1,9,15,15,10,12,5,9,5,19,15,9,1,5,3,4,1,5,3,4,9,5,9,5,9,5,15,10,15,10,15,10,5,15,10]
-    # v = [9,1,5,3,4,1,5,3,4,9,5,9,5,9,5,15,10,15,10,15,10,5,15,10,7,4,3,4,5,6,7,8,8,9,9,5,1,1,9,15,15,10,12,5,9,5,19,15]
+    w = [7,4,3,4,5,6,7,8,8,9,9,5,1,1,9,15,15,10,12,5,9,5,19,15,9,1,5,3,4,1,5,3,4,9,5,9,5,9,5,15,10,15,10,15,10,5,15,10]
+    v = [9,1,5,3,4,1,5,3,4,9,5,9,5,9,5,15,10,15,10,15,10,5,15,10,7,4,3,4,5,6,7,8,8,9,9,5,1,1,9,15,15,10,12,5,9,5,19,15]
     
     # res = maxVal(w,v,len(v)-1,n)
     # print 'max val =', res, 'number of calls = ',numCalls
--- ps8 study pad.py
+++ ps8 study pad.py
@@ -13,7 +13,7 @@
 import time
 
 SUBJECT_FILENAME = "my_subjects.txt"
-#SUBJECT_FILENAME = "subjects.txt"
+SUBJECT_FILENAME = "subjects.txt"
 
 VALUE, WORK = 0, 1
 
@@ -38,7 +38,6 @@ def loadSubjects(filename):
         result[line_as_list[0]] = (int(line_as_list[-2]),int(line_as_list[-1])) 
     return result
 
-
 def printSubjects(subjects):
     """
     Prints a string containing name, value, and work of each subject in
@@ -59,48 +58,6 @@ def printSubjects(subjects):
     res = res + '\nTotal Value:\t' + str(totalVal) +'\n'
     res = res + 'Total Work:\t' + str(totalWork) + '\n'
     print res
-
-def bruteForceAdvisor(subjects, maxWork):
-    """
-    Returns a dictionary mapping subject name to (value, work), which
-    represents the globally optimal selection of subjects using a brute force
-    algorithm.
-    
-    subjects: dictionary mapping subject name to (value, work)
-    maxWork: int >= 0
-    returns: dictionary mapping subject name to (value, work)
-    """
-    nameList = subjects.keys()
-    tupleList = subjects.values()
-    bestSubset, bestSubsetValue = \
-            bruteForceAdvisorHelper(tupleList, maxWork, 0, None, None, [], 0, 0)
-    outputSubjects = {}
-    for i in bestSubset:
-        outputSubjects[nameList[i]] = tupleList[i]
-    return outputSubjects
-
-def bruteForceAdvisorHelper(subjects, maxWork, i, bestSubset, bestSubsetValue, subset, subsetValue, subsetWork):
-    # Hit the end of the list.
-    if i >= len(subjects):
-        if bestSubset == None or subsetValue > bestSubsetValue:
-            # Found a new best.
-            return subset[:], subsetValue
-        else:
-            # Keep the current best.
-            return bestSubset, bestSubsetValue
-    else:
-        s = subjects[i]
-        # Try including subjects[i] in the current working subset.
-        if subsetWork + s[WORK] <= maxWork:
-            subset.append(i)
-            bestSubset, bestSubsetValue = bruteForceAdvisorHelper(subjects,
-                    maxWork, i+1, bestSubset, bestSubsetValue, subset,
-                    subsetValue + s[VALUE], subsetWork + s[WORK])
-            subset.pop()
-        bestSubset, bestSubsetValue = bruteForceAdvisorHelper(subjects,
-                maxWork, i+1, bestSubset, bestSubsetValue, subset,
-                subsetValue, subsetWork)
-        return bestSubset, bestSubsetValue
 
 def dpAdvisor(subjects, maxWork):
     """
@@ -140,13 +97,13 @@ def dpAdvisorHelper(valueWorkList, i, aWork, memo) :
     selectedIndexes:    list of the indexes of the selected elements in
                         valueWorkList
     """
+    # print '\nIn dpAdvisorHelper.'   ,'\nvalueWorkList', valueWorkList
+    # print 'i:', i,
+    # print 'a:', aWork,
+    # print 'v[i]', valueWorkList[i],
+    # print '\tmemo:', memo
     
     ##trying to make use of already computed values
-    # print '\nIn dpAdvisorHelper.'   ,'\nvalueWorkList', valueWorkList
-    print 'i:', i,
-    print 'a:', aWork,
-    print 'v[i]', valueWorkList[i],
-    print '\tmemo:', memo
     try : return memo[(i, aWork)]
     except KeyError :
         ith_value = valueWorkList[i][VALUE]
@@ -164,124 +121,22 @@ def dpAdvisorHelper(valueWorkList, i, aWork, memo) :
                 return 0, selectedIndexes        
         ##somewhere in the middle of the decision tree,
         ##computing value without the i-th element
-        without_i, selectedIndexes =\
-                   dpAdvisorHelper(valueWorkList, i-1, aWork, memo) ##left branch, do not take
+        without_i, selectedIndexes = dpAdvisorHelper(valueWorkList, i-1, aWork, memo) ##left branch, do not take
+        
         if ith_workload > aWork :
             memo[(i, aWork)] = without_i, selectedIndexes
             return without_i, selectedIndexes ##commited not to take the element
         else : ##compute value with the i-th element
-            with_i, I =\
-                    dpAdvisorHelper(valueWorkList, i-1, aWork-ith_workload, memo)
-            ##ma sti cazzi!!! three days lost because of this fuckin' I.
-            ##the erred one was with_i, selectedIndexes =...
-            ##by the way if anyone could kindly explain me why this old version
-            ##didn't work i'd be very thankful.
-            ##The other version had an if we can take it, take it branch.  But the don't
-            ##take branch wasn't under and else: statement.  So it ran everytime even
-            ##after the IF recursion.  I'm not sure at all why this is right and the other way
-            ##was wrong, but it's hard to argue with success.
-            ## I think if you use with_i, selectedIndexes = ... on the left side, then
-            ## you have to have some tuple additions on the right side. For example, you want
-            ## with_i = ith_value + the first part of dpAdvisorHelper and you want
-            ## selectedIndexes = [i] + the second part of dpAdvisorHelper.
-            ## Something like this:
-            ##    with_i, selectedIndexes =\
-            ##        ith_value, [i] + dpAdvisorHelper(valueWorkList, i-1, aWork-ith_workload, memo)
-            ## But this doesn't work because you can't add tuples together like this.
-            ## So I agree, 'ma sti cazzi!', working with tuples is so much more complex than
-            ## working with integers!
-            ##
-            ## Wait a minute! Writing out this comment, I thought of another way to do this.
-            ## You can add the parts of the tuples by separating them out, like this:
-            ##
-            ## with_i, selectedIndexes =\
-            ##     ith_value + dpAdvisorHelper(valueWorkList, i-1, aWork-ith_workload, memo)[0],\
-            ##     [i] + dpAdvisorHelper(valueWorkList, i-1, aWork-ith_workload, memo)[1]
-            ##
-            ## Remember to comment out the with_i += ith_value below along with the
-            ## selectedIndexes = [i] + I in the if block below.
-            ## These additions are done in the above statement.
-            ##
-            ## It does work, but the time, dpTime(), is about 10x longer than what is here.
+            with_i, I = dpAdvisorHelper(valueWorkList, i-1, aWork-ith_workload, memo)
             with_i += ith_value
         ##compare the values with and without i-th element
         if with_i >= without_i :
-            ##take the i-th element
-            #for keeping track of the indexes, why does only the last of these work properly?
-            #selectedIndexes = I
-            ##selectedIndexes.append(i)
-            ##selectedIndexes.extend([i])
-            ##selectedIndexes += [i]
-            ###selectedIndexes = selectedIndexes + [i]
-            ##Now I'm not 100% here, but I think that by concatenating the lists you're creating
-            ##a new one, rather than a reference to one.  Like you need to use dict.copy() if you
-            ##want a seperate instance of a data structure and not just a pointer.
-            ## http://diveintopython.org/getting_to_know_python/lists.html
-            ## 1) Lists can also be concatenated with the + operator. list = list + otherlist has the same
-            ## result as list.extend(otherlist). But the + operator returns a new (concatenated) list as a
-            ## value, whereas extend only alters an existing list. This means that extend is faster,
-            ## especially for large lists.
-            ## 2) Python supports the += operator. li += ['two'] is equivalent to li.extend(['two']).
             selectedIndexes = [i] + I
             maxValue = with_i
         else : ##do not take the i-th element
             maxValue = without_i
         memo[(i, aWork)] = maxValue, selectedIndexes
         return maxValue, selectedIndexes
-# Thank you for posting your solution to this, Kamenoko. I struggled with it for quite a while and 
-# just wanted to share what I figured out, in case anyone else has the same issues and to see if
-# anyone can add any further insight to help me understand the error of my ways.
-# My original approach to this was to attempt to add memoization to the brute force solution that
-# was provided with the problem set. The brute force algorithm starts by selecting the early items,
-# adds items until the max is reached, and recurses to the end of the list before a solution is
-# found. I believe that the problem with memoization here is that you end up capturing the first
-# answer for a given index/available weight, and it is not necessarily the best answer. On the other
-# hand, the solution posted above starts at the end of the list and works it's way back, so that as
-# each new index is considered, it can refer to the memoized solutions, which are in fact the
-# optimal solutions. Does that sound right?
-# If anyone else has any other thoughts or theories on this please chime in. Thanks again for
-# posting this. - BriChri
-    
-# @BriChri... the solution below adds memoization to the brute force solution (just # a note it uses the exact same params as the brute force algorithm). The problem   # was as you noticed that you capture the first answer to a given index / available # weight but not necessarily the best answer. After adding a bunch of print         # statements I noticed that the best answer is computed, just never gets stored
-# in the memo. So I added the extra if statement in the try block. This insures that
-# the correct value gets stored. However, because of the extra check this ends up
-# being slightly slower than the algorithm above, but still faster than the brute
-# force method   
-    
-    availableWork = maxWork - subsetWork
-    
-    try:
-        if (memo[(i, availableWork)][1] > subsetValue):
-            return memo[(i, availableWork)]
-        else:
-            raise KeyError
-    except KeyError:
-        if i >= len(valueWorkList): # a leaf node... end of the line.
-            if bestSubset == None or subsetValue > bestSubsetValue:
-                memo [(i, availableWork)] = subset[:], subsetValue
-                return subset[:] , subsetValue
-            else:
-                memo [(i, availableWork)] = bestSubset, bestSubsetValue
-                return bestSubset, bestSubsetValue
-            
-        else:
-            s = valueWorkList[i]
-            if s[WORK] <= availableWork:
-                # Take branch
-                subset.append(i)
-                bestSubset, bestSubsetValue = dpAdvisorHelper(valueWorkList,
-                        maxWork, i+1, bestSubset, bestSubsetValue, subset,
-                        subsetValue + s[VALUE], subsetWork + s[WORK], memo)
-                subset.pop() # revert (remove the last i)
-            
-            # Do not take branch
-            bestSubset, bestSubsetValue = dpAdvisorHelper(valueWorkList,
-                    maxWork, i+1, bestSubset, bestSubsetValue, subset,
-                    subsetValue, subsetWork, memo)
-        
-        memo [(i, availableWork)] = bestSubset, bestSubsetValue
-        return bestSubset, bestSubsetValue
-
 
 def dpTime():
     """
@@ -291,7 +146,7 @@ def dpTime():
     # TODO...
     subjects = loadSubjects(SUBJECT_FILENAME)
     # print "subjects:", subjects
-    test_maxWork = [4]
+    test_maxWork = [30]
     for each in test_maxWork:
         print "maximum workload", each, '\n',
         startTime = time.time()
