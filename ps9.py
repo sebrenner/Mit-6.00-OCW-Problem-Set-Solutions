fbcbc258dcf38925467cd465887de4c1d761d6d6
Scott Brenner <scott@scottbrenner.com>
2011-03-04 22:18:54 -0500

    ps9: Finished problem 2.

--- ps9.py
+++ ps9.py
@@ -9,6 +9,8 @@ from string import *
 class Shape(object):
     def area(self):
         raise AttributeException("Subclasses should override this method.")
+        
+    
 
 class Square(Shape):
     def __init__(self, h):
@@ -63,26 +65,28 @@ class Triangle(Shape):
         """
         self.base = float(base)
         self.height = float(height)
-
+    
+    
     def area(self):
         """
         Returns approximate area of the triangle 
         """
         return .5 * self.base * self.height
-        
+    
     def __str__(self):
         """
         Describes this intance of Triangle.
         """
         return 'Triangle with a base of %0.2f and height of %0.2f.' % (self.base, self.height)
-        
+    
     def __eq__(self, other):
         """
         Two triangles are equal if they have the same base and height.
         other: object to check for equality
         """
         return type(other) == Triangle and self.base == other.base and self.height == other.height
-        
+    
+    
 
 #
 # Problem 2: Create the ShapeSet class
@@ -96,6 +100,7 @@ class ShapeSet:
         Initialize any needed variables
         """
         self.members = []
+        self.index = 0
         ## TO DO
     
     def addShape(self, sh):
@@ -105,17 +110,30 @@ class ShapeSet:
         sh: shape to be added
         """
         ## TO DO
-        self.members.append(sh)
+        if sh not in self.members:
+            self.members.append(sh)
     
+    def __getitem__(self, int):
+        """
+        Returns the shape at the given index
+        """
+        return self.members[int]
+
     def __iter__(self):
         """
         Return an iterator that allows you to iterate over the set of
         shapes, one shape at a time
         """
         ## TO DO
-        for shape in self.members:
-            return shape
-               
+        return self
+    
+    def next(self):
+        if self.index == len(self.members):
+            raise StopIteration
+        current_index = self.index
+        self.index = self.index + 1
+        return self.members[current_index]
+    
     def __str__(self):
         """
         Return the string representation for a set, which consists of
@@ -123,9 +141,19 @@ class ShapeSet:
         (circles, then squares, then triangles)
         """
         ## TO DO
-        result = 'This list contains the following shapes:',
-        for each in self:
-            result += each
+        taco = ''
+        for each in my_shapeset.members:
+            taco = taco + "\n" + each.__str__()
+        return taco
+        
+        
+        
+        
+        result =''
+        for each in self.members:
+            result = result + '\n' + each.__str__()
+            print each.__str__()
+	    return result 
     
 
 #
@@ -133,39 +161,51 @@ class ShapeSet:
 #
 def findLargest(shapes):
     """
-    Returns a tuple containing the elements of ShapeSet with the
-       largest area.
+    Returns a tuple containing the elements of ShapeSet with the largest area.
+
     shapes: ShapeSet
     """
     ## TO DO
     pass
     
- 
+    
+
 #
 # Problem 4: Read shapes from a file into a ShapeSet
 #
-def readShapesFromFile(filename):
-    """
-    Retrieves shape information from the given file.
-    Creates and returns a ShapeSet with the shapes found.
-    filename: string
+
+def readShapesFromFile(filename): """Retrieves shape information from the given file. Creates and returns a ShapeSet with the shapes found. filename: string
     """
     ## TO DO
 
 
-my_triangle = Triangle(6,7)
-your_triangle = my_triangle
-her_triangle = Triangle(6,7)
+
+my_circle = Circle(2)
+my_square = Square(4)
+my_square2 = Square(1)
+my_triangle = Triangle(1,1)
+
 
 my_shapeset = ShapeSet()
 
+# 
+# print my_triangle
+# print my_triangle.area()
+# print your_triangle == my_triangle
+# print your_triangle == her_triangle
+# your_triangle.base = 45
+# print my_triangle
 
-print my_triangle
-print my_triangle.area()
-print your_triangle == my_triangle
-print your_triangle == her_triangle
-your_triangle.base = 45
-print my_triangle
 
-my_shapeset.addShape(my_triangle)
-print my_shapeset
+my_shapeset.addShape(Circle(2))
+my_shapeset.addShape(Square(4))
+my_shapeset.addShape(Square(1))
+my_shapeset.addShape(Triangle(1,1))
+
+#print my_shapeset.members
+#print my_shapeset.next()
+# print my_shapeset.index
+# print my_shapeset.next()
+# print my_shapeset.index
+
+print my_shapeset.__str__()
